b807f93c2409d8dc609b0f187873e9553764a5f0
toumasaya <luffy.w.pirate@gmail.com>
2017-04-22 15:51:04 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 let p = document.createElement('p');
 document.appendChild(p);
-p.innerHTML("GG Queen")
+p.innerHTML("GG Queen");
+
+let button = document.getElementById('button');
+button.addEventListener('click', () => {
+  alert('GG');
+});
